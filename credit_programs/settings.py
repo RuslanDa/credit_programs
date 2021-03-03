1978991bef89c4b42946c44f9a0c5dfde396bab5
Ruslan Davletshin <ru@ru.ru>
2021-03-03 11:13:07 +0500

    changed settings for start "credits" app

--- credit_programs/settings.py
+++ credit_programs/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'credits.apps.CreditsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
